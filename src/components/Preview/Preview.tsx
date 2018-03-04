ffde41eb726ac75d7fb5bc91d845d7c3f5ea88a4
Utkarsh Saxena <utkarshsaxena93@gmail.com>
2018-03-04 12:58:02 -0500

    add more folders

---
+++ src/components/Preview/Preview.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class Preview extends React.Component {
+  render() {
+    return 'Design Mode';
+  }
+}
+
+export default Preview;
---
+++ src/components/TextEditor/TextEditor.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+class TextEditor extends React.Component {
+  render() {
+    return 'Text Editor';
+  }
+}
+
+export default TextEditor;
